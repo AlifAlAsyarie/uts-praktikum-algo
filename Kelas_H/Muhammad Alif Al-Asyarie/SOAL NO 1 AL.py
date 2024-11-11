d6b1dca968b70fd7c26ef5fe451168b866abf308
AlifAlAsyarie <2403010189@unper.ac.id>
2024-11-11 16:07:08 +0700

    Muhammad Alif Al-Asyarie_2403010189

---
+++ Kelas_H/Muhammad Alif Al-Asyarie/SOAL NO 1 AL.py
@@ -0,0 +1,13 @@
+# Menghitung Luas dan Keliling Persegi Panjang
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+print(f"Luas persegi panjang: {luas}")
+print(f"Keliling persegi panjang: {keliling}")
+# Keterangan
+# ini adalah cara untuk menghitung Luas dan Keliling Persegi Panjang dengan menggukan codingan
+# Nama: Muhammad Alif Al-Asyarie
+# NIM: 2403010189
