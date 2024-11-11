1970d379dfd51367740ededf06a3033e313a12b2
AlifAlAsyarie <2403010189@unper.ac.id>
2024-11-11 15:51:48 +0700

    Muhammad Alif Al-Asyarie_2403010189

---
+++ Kelas_H/Muhammad Alif Al-Asyarir_2403010189/SOAL NO 2 AL.py
@@ -0,0 +1,11 @@
+# Konversi Suhu Celcius ke Fahrenheit
+celcius = float(input("Masukkan suhu dalam Celcius: "))
+
+fahrenheit = (celcius * 9/5) + 32
+
+print(f"Suhu dalam Fahrenheit: {fahrenheit}")
+# Keterangan
+# Ini adalah cara memasukan suhu ke dalam celcius dengan menggukan coding
+# Nama: Muhammad Alif Al-Asyarie
+# NIM: 2403010189
+ 
